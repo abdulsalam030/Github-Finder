4a16211cdf471a4edfca5bb537f5778bc1162563
abdulsalam030 <abdulsalamopeyemi030@gmail.com>
2023-03-03 10:29:49 +0100

    Home and About Links added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,6 +11,19 @@ function Navbar({title="Github Profile Finder"}) {
               {title}
             </Link>
         </div>
+        <div className='flex-1 px-2 mx-2' >
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+            Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+            About
+            </Link>
+          </div>
+
+        </div>
+
+
     </div>
 
    </nav>
